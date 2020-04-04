d9fba39979a7d1f3d54de6e09d20eb78b27d1805
amaloun <naima.amalou@grenoble-inp.org>
2020-04-04 18:48:51 +0200

    add creation tables

--- projetAcol/install_bd.sql
+++ projetAcol/install_bd.sql
@@ -7,9 +7,39 @@
  * Author:  amalou
  * Created: 24 mars 2020
  */
+CREATE SEQUENCE id_seq;
 
-create table utilisateur
+create table Utilisateur
 (
-    pseudonyme VARCHAR(20) primary key not null,
-    password VARCHAR(60) not null,
-) 
+    pseudonyme NVARCHAR2(20) primary key not null,
+    password NVARCHAR2(60) not null,
+    email NVARCHAR2(60) not null
+); 
+
+create table Partie
+(
+    idPartie number(6) DEFAULT id_seq.nextval PRIMARY KEY,
+    nbrPart  INT(3)  not null,
+    dureeJour float(10) not null,
+    dureeNuit float(10) not null,
+    heureDeb DATE not null,
+    probaPouvoir float(10) not null,
+    propLoup float(10) not null
+);
+
+create table Joeur
+(
+    pseudonyme NVARCHAR2(20) not null references Utilisateur(pseudonyme),
+    idPartie number(6) not null references Partie(idPartie),
+    primary key(pseudonyme,idPartie)
+    
+);
+
+create table Message
+(
+    idPartie number(6) not null references Partie(idPartie),
+    datePub DATE not null,
+    pseudonyme NVARCHAR2(20) not null references Utilisateur(pseudonyme),
+    contenu NVRCHAR(10000) not null,
+    primary key(idPartie,datePub,pseudonyme)
+);
